b30b54f3ef277058d6814842588d13dd76de7a4e
Aiko Beniyama <saltory72@gmail.com>
2022-09-01 15:58:34 +0900

    wisdom-guildへの接続APIのひな型
    wisdom guildへの接続APIのひな型クラスを作成。

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,5 +1,39 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import axios from "axios";
+let value = ref("");
+onMounted(async () => {
+    console.log("mounted");
+    await axios
+        .get("/api/wisdom")
+        .then((response) => {
+            console.log(response);
+            value.value = response.data;
+        })
+        .catch((e) => {
+            console.error(e);
+        });
+});
+</script>
+
 <template>
     <h1 class="display-6">カード登録</h1>
     <small class="text-muted">Wisdom Guildからカード情報を持ってきます</small>
-    <div>Indexxxx</div>
+    {{ value.name }}
+    <table class="table table-striped table-bordered mt-3">
+        <thead class="">
+            <tr>
+                <th scope="col">カード番号</th>
+                <th scope="col">カード名</th>
+                <th scope="col">英語名</th>
+                <th scope="col">価格</th>
+            </tr>
+        </thead>
+        <tr>
+            <!-- <td>{{$card->getIndex()}}</td>
+            <td>{{$card->getName()}}</td>
+            <td>{{$card->getEnname()}}</td>
+            <td>{{$card->getPrice()}}</td> -->
+        </tr>
+    </table>
 </template>
