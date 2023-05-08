3f0dc3c0eedfe9e0a961059985b6a12fc94fb36f
Aiko Beniyama <saltory72@gmail.com>
2023-05-08 11:50:31 +0900

    #87 注文用CSVファイルから注文番号と住所、氏名を抽出する処理を追加。

--- resources/js/pages/packing/Packing.vue
+++ resources/js/pages/packing/Packing.vue
@@ -1,31 +1,67 @@
 <template>
-    <p>注文用CSVファイルをアップロードしてください。</p>
-    <csv-upload @upload="orderUpload">アップロード</csv-upload>
-    <table class="ui table striped">
-        <thead>
-            <tr>
-                <th>注文番号</th>
-                <th>商品名</th>
-                <th>住所</th>
-                <th>氏名</th>
-            </tr>
-        </thead>
-        <tr>
-            <td>order_GcaZ3wmfaF2sS7zzmJRQqX</td>
-            <td>【MOM】救済の波濤[JP][白]</td>
-            <td>〒852-8035 長崎県長崎市油木町41-160 井手口アパート202号室</td>
-            <td>山見 啓太様</td>
-        </tr>
-    </table>
+    <message-area></message-area>
+    <div class="ui message">
+        <p>注文用CSVファイルをアップロードしてください。</p>
+    </div>
+    <csv-upload @upload="orderUpload">アップロード</csv-upload
+    ><label class="ml-1">{{ filename }}</label>
+    <div class="mt-2">
+        <table class="ui table striped">
+            <thead>
+                <tr>
+                    <th>注文番号</th>
+                    <th>商品名</th>
+                    <th>住所</th>
+                    <th>氏名</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="o in orders">
+                    <td>{{ o.order_id }}</td>
+                    <td>【MOM】救済の波濤[JP][白]</td>
+                    <td>{{ o.address }}</td>
+                    <td>{{ o.name }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 <script>
+import Encoding from "encoding-japanese";
+import MessageArea from "../component/MessageArea.vue";
 import CSVUpload from "../component/CSVUpload.vue";
 export default {
+    data() {
+        return {
+            filename: "",
+            orders: [],
+        };
+    },
     methods: {
+        // アップロードイベント
         orderUpload: function (file) {
+            this.$papa.parse(file, {
+                header: true,
+                complete: function (results) {
+                    this.filename = file.name;
+                    let data = results.data;
+                    data.map((line) => {
+                        let order = {};
+                        order["order_id"] = line["order_id"];
+                        order[
+                            "address"
+                        ] = `〒${line["shipping_postal_code"]} ${line["shipping_state"]}${line["shipping_city"]}${line["shipping_address_1"]} ${line["shipping_address_2"]}`;
+                        order["name"] = `${line["shipping_name"]}様`;
+                        this.orders.push(order);
+                    });
+                    // console.log(line["order_id"]);
+                    // this.contentMap[] = line["商品ID"];
+                    // });
+                }.bind(this),
+            });
             console.log("OK");
         },
     },
-    components: { "csv-upload": CSVUpload },
+    components: { "csv-upload": CSVUpload, "message-area": MessageArea },
 };
 </script>
