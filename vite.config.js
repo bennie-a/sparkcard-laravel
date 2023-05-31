b6f959ed7fe8b203c9b7cf36267949372a7df014
Aiko Beniyama <saltory72@gmail.com>
2023-05-31 17:06:12 +0900

    #92 jsファイルとcssファイルをビルドした時の設定を追加。

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     base: "./",
     plugins: [
         laravel({
-            input: ["resources/sass/app.scss", "resources/js/app.js"],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         vue(),
@@ -19,9 +19,6 @@ export default defineConfig({
     },
     server: {
         port: 8082,
-        host: true,
-        hmr: {
-            host: "localhost",
-        },
+        host: "localhost",
     },
 });
