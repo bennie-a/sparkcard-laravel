b6ff60a202a02b412aca750a2947498e2a143a83
Aiko Beniyama <saltory72@gmail.com>
2025-10-06 10:56:33 +0900

    fix #258

--- tests/Unit/Scryfall/ScryfallTest.php
+++ tests/Unit/Scryfall/ScryfallTest.php
@@ -31,6 +31,8 @@ class ScryfallTest extends TestCase
     {
         return [
             '表面のみ' => ['IKO', 1, 'https://cards.scryfall.io/png/front/e/1/e1059d5b-1de6-4988-a3a8-fe540a541342.png?1645734158'],
+            '上下二面カード' => ['AKH', 210, 'https://cards.scryfall.io/png/front/4/c/4cffc5c9-0115-4a8f-9665-a3fbdd4179c2.png?1540281378'],
+            '表裏両面カード' => ['MH3', 261, 'https://cards.scryfall.io/png/front/3/0/305ae3b5-7c12-43ad-b19f-dbd04c9afcf7.png?1730229671'],
         ];
     }
 }
