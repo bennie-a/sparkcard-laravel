04e665d66f41debb75b6b938d78de8a1f6b6b8fd
Aiko Beniyama <saltory72@gmail.com>
2026-02-26 11:28:08 +0900

    :wrench: :test_tube: #270 出荷情報解析APIのバリデーションを変更。

--- app/Http/Validator/ShiptValidator.php
+++ app/Http/Validator/ShiptValidator.php
@@ -39,7 +39,6 @@ class ShiptValidator extends AbstractCsvValidator {
    protected function attributes():array {
         return [
             ShiptCon::PRODUCT_PRICE => '商品価格',
-            ShiptCon::DISCOUNT_AMOUNT => '割引額',
             ShiptCon::SINGLE_PRICE => '単価',
             ShiptCon::TOTAL_PRICE => '支払い価格',
             ShiptCon::PRODUCT_ID => '商品コード',
--- tests/Unit/DB/Shipt/ShiptParseTest.php
+++ tests/Unit/DB/Shipt/ShiptParseTest.php
@@ -405,25 +405,6 @@ class ShiptParseTest extends TestCase
             EC::DETAIL => 'ファイルはCSV形式でアップロードしてください']);
     }
 
-    #[Test]
-    #[TestDox('商品コードが存在しない場合、行数とメッセージが返ってくるか検証する。')]
-    public function ngNoProductId() {
-        $buyerInfo = ShiptLogTestHelper::createTodayOrderInfos();
-        $buyerInfo[SC::ITEMS][0][GC::ID] = '9999';
-        $implode = $this->createCsvLine([$buyerInfo]);
-        $header = ShiptLogTestHelper::getHeader();
-        $content = <<<CSV
-        {$header}
-        {$implode}
-        CSV;
-
-        $this->setMockCardBoard([$buyerInfo[SC::ORDER_ID]]);
-        $status = CustomResponse::HTTP_CSV_VALIDATION;
-
-        $response = $this->upload($content, $status);
-        $this->assertRowError($response, $status, '商品コードが存在しません。');
-    }
-
     #[TestDox('不正な商品情報がある場合、行数とメッセージが返ってくるか検証する。')]
     #[TestWith([SC::ORDER_ID, 'error', 'no-notion'], '注文番号が入力されたNotionカードが存在しない')]
     #[TestWith([SC::QUANTITY, '999', 'excess-shipment'], '出荷枚数が在庫枚数より多い')]
