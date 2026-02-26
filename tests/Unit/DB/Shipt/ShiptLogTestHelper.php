afae5fb1d528a3d3a0aa8a7a3f0739f2a0c5042e
Aiko Beniyama <saltory72@gmail.com>
2026-02-26 11:07:13 +0900

    Revert ":wastebasket: :rotating_light: #270 出荷情報解析APIとそのテストクラスから「発送日」を削除。"
    This reverts commit dd680d2d7d0089bbf2001895b9ba9558c4c0775a.

--- tests/Unit/DB/Shipt/ShiptLogTestHelper.php
+++ tests/Unit/DB/Shipt/ShiptLogTestHelper.php
@@ -69,7 +69,7 @@ class ShiptLogTestHelper
      * @param string $shiptDate
      * @return array
      */
-    public static function createBuyerInfoOnly():array {
+    public static function createBuyerInfoOnly(string $shiptDate):array {
         return [
             SC::ORDER_ID => self::createOrderId(),
             SC::BUYER => fake()->name(),
@@ -78,6 +78,7 @@ class ShiptLogTestHelper
             SC::CITY => fake()->city(),
             SC::ADDRESS_1 => fake()->streetAddress(),
             SC::ADDRESS_2 => fake()->secondaryAddress(),
+            SC::SHIPPING_DATE => $shiptDate,
         ];
     }
 
