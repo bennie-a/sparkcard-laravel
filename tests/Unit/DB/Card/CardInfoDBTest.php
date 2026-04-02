8a98fccf358fffee0eb81bff4fe206a2655217ae
Aiko Beniyama <saltory72@gmail.com>
2026-04-02 09:58:34 +0900

    :test_tube: #302 アート・カード関連のテストを追加。

--- tests/Unit/DB/Card/CardInfoDBTest.php
+++ tests/Unit/DB/Card/CardInfoDBTest.php
@@ -8,9 +8,7 @@ use App\Models\CardInfo;
 use App\Models\Expansion;
 use App\Models\Foiltype;
 use App\Services\Constant\CardConstant as Con;
-use App\Services\Constant\CardConstant;
 use App\Services\Constant\GlobalConstant;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
 use Tests\Database\Seeders\DatabaseSeeder;
 use Tests\Database\Seeders\TestCardInfoSeeder;
@@ -22,8 +20,10 @@ use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertNotNull;
 use function PHPUnit\Framework\assertNull;
 use function PHPUnit\Framework\assertSame;
-use Illuminate\Support\Str;
 use PHPUnit\Framework\Attributes\DataProvider;
+use App\Services\Constant\GlobalConstant as GCon;
+use GMP;
+use PHPUnit\Framework\Attributes\TestWith;
 
 /**
  * カード情報を登録するテストクラス
@@ -100,7 +100,7 @@ class CardInfoDBTest extends TestCase
             $exp = Expansion::findBySetCode($setcode);
             $this->assertDatabaseHas('card_info', [
                 Con::EXP_ID => $exp->notion_id,
-                GlobalConstant::NAME => $params[GlobalConstant::NAME],
+                GCon::NAME => $params[GCon::NAME],
                 Con::EN_NAME => $params[Con::EN_NAME],
                 'color_id' => $params[Con::COLOR],
                 Con::NUMBER => $params[Con::NUMBER],
@@ -114,13 +114,14 @@ class CardInfoDBTest extends TestCase
         return [
             '通常版' => ['BRO', '出現領域', 1, ['通常版']],
             '特別版_共通' => ['NEO', '発展の暴君、ジン＝ギタクシアス', 2, ['通常版', 'Foil']],
-            '特別版_セット特有' => ['MOM', '族樹の精霊、アナフェンザ', 13, ['ハロー・Foil']]
+            '特別版_セット特有' => ['MOM', '族樹の精霊、アナフェンザ', 13, ['ハロー・Foil']],
+            '箔押し' => ['MOM', 'アート・カード', 1, ['通常版', '箔押し']],
         ];
     }
 
     private function createParams(string $setcode, string $name, int $promotypeId, array $foiltype): array{
         $data = ['setCode' => $setcode,
-                            Con::NAME => $name,
+                            GCon::NAME => $name,
                             Con::EN_NAME => $this->getEnName(),
                             Con::COLOR => fake()->randomElement(['U', 'B', 'R', 'G', 'W', 'Land']),
                             Con::NUMBER => fake()->numberBetween(1, 400),
@@ -129,40 +130,24 @@ class CardInfoDBTest extends TestCase
         return $data;
     }
 
-    private function post_ok($data)
-    {
-        // $this->markTestSkipped('一時スキップ');
-        $this->post_execute($data, 201);
+    #[TestWith(['Art'], 'アート・カード')]
+    #[TestDox('カードの色の登録に関する検証')]
+    public function test_color(string $color) {
+        $setcode = 'BRO';
+        $name = fake()->realText(10);
+        $params = $this->createParams($setcode, $name, 1, ['通常版']);
+        $params[Con::COLOR] = $color;
+        $this->post_execute($params, 201);
 
-        $setcode = $data['setCode'];
-        $exp = Expansion::where('attr', '=', $setcode)->first();
-        $number = $data[Con::NUMBER];
-        $record = CardInfo::where(['exp_id' => $exp->notion_id, 'number' => $number])->get();
-        $foiltype =  $data[Con::FOIL_TYPE];
-        assertCount(count($foiltype), $record, '登録レコードの有無');
-
-        foreach($foiltype as $foil) {
-            $type = Foiltype::findByName($foil);
-            assertNotNull($type);
-            $actual = $record->first(function($item) use ($type) {
-                return $item->foiltype_id == $type->id;
-            });
-            assertNotNull($actual, $foil);
-
-            $name =  $data[Con::NAME];
-            $name = empty($data[Con::PROMOTYPE]) ? $name : $name.'≪'.$data[Con::PROMOTYPE].'≫';
-            assertEquals($name, $actual->name, 'カード名');
-            assertEquals($data[Con::EN_NAME], $actual->en_name, 'カード名(英名)');
-            assertEquals($data[Con::COLOR], $actual->color_id, '色');
-            assertEquals($data[Con::NUMBER], $actual->number, 'カード番号');
-            $isFoil = $foil != '通常版';
-            assertSame($isFoil, $actual->isFoil, 'Foilフラグ');
-            if ($data['multiverseId'] != 0 || !empty($data['scryfallId'])) {
-                assertNotNull($actual->image_url, '画像URL');
-            } else {
-                assertNull($actual->image_url, '画像URL');
-            }
-        }
+        $exp = Expansion::findBySetCode($setcode);
+        $this->assertDatabaseHas('card_info', [
+            Con::EXP_ID => $exp->notion_id,
+            GCon::NAME => $params[GCon::NAME],
+            Con::EN_NAME => $params[Con::EN_NAME],
+            'color_id' => $params[Con::COLOR],
+            Con::NUMBER => $params[Con::NUMBER],
+            Con::PROMO_ID => 1
+        ]);
     }
 
     private function post_execute($data, int $statuscode) {
