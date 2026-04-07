6e38a1d30d63d706d0707a233bd07f959a1b572d
Aiko Beniyama <saltory72@gmail.com>
2026-04-07 10:31:04 +0900

    :books: バージョンを「5.3.0」に変更。 :art: #304 BASE API連携画面にヘッダーを追加。

--- resources/js/pages/baseshop/BaseApiIntegration.vue
+++ resources/js/pages/baseshop/BaseApiIntegration.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="login-page">
-    <h1>ログイン</h1>
-    <!-- フォーム -->
-  </div>
+    <header>
+        <div class="ui pointing">
+            <div class="header navbar-brand">
+                <router-link to="/" class="navbar-brand"
+                    ><i class="bi bi-gem"></i> SPARKCARD</router-link
+                >
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="login-page">
+          <h1>ログイン</h1>
+          <!-- フォーム -->
+        </div>
+    </main>
+    <footer>bbb</footer>
 </template>
