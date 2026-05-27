7df0da8b965676d8a1484b3c887969a15281e52f
Aiko Beniyama <saltory72@gmail.com>
2026-05-27 11:31:16 +0900

    :art: #317 注文一括登録画面 ・CSSをリファクタリングするため仮データを作成。

--- resources/js/pages/mercari/ShiptMercariImpPage.vue
+++ resources/js/pages/mercari/ShiptMercariImpPage.vue
@@ -9,17 +9,264 @@
     import ModalButton from '../component/modal/ModalButton.vue';
     import PiniaMsgForm from '../component/PiniaMsgForm.vue';
     import scdatepicker from "../component/SCDatePicker.vue";
+    import ListPagination from "@/pages/component/pagination/ListPagination.vue";
+    // import { usePagenate } from "./component/pagination/UsePaginate";
+    // import {MsgStore} from "./component/msg/MsgStore";
 
+    // const msgStore = MsgStore();
 
     const result = reactive([]);
     const resultCount = ref(0);
     const isLoading = ref(false);
-    const currentList = reactive([]);
+    // const currentList = reactive([]);
 
     const error = reactive([]);
     const hasError = ref(false);
-    const hasResult = ref(false);
+    const hasResult = ref(true);
     const shiptDate = ref(new Date());
+
+    const currentList = ref([
+  {
+    "order_id": "order_A9xKpLm2Qw7RsT8VbNcD1E",
+    "buyer_name": "佐藤 美咲",
+    "zip_code": "150-0043",
+    "address": "東京都渋谷区道玄坂2-24-1",
+    "shipping_fee": 185,
+    "total_price": 1280,
+    "items": [
+      {
+        "stock": {
+          "id": 4127,
+          "card": {
+            "name": "稲妻",
+            "exp": {
+              "name": "基本セット2010",
+              "attr": "M10"
+            },
+            "number": "146",
+            "image_url": "https://cards.scryfall.io/large/front/4/3/435589bb-27c6-4a6d-9d63-394d5092b9d8.jpg?1561978182",
+            "foil": {
+              "is_foil": false,
+              "name": ""
+            },
+            "promotype": {
+              "id": 1,
+              "name": ""
+            }
+          },
+          "condition": "NM",
+          "lang": "EN",
+          "quantity": 4
+        },
+        "shipment": 3,
+        "single_price": 250,
+        "total_price": 1000,
+        "coupon_discount_amount": 0,
+        "isRegistered": false
+      },
+      {
+        "stock": {
+          "id": 4128,
+          "card": {
+            "name": "対抗呪文",
+            "exp": {
+              "name": "ストリクスヘイヴン：ミスティカルアーカイブ",
+              "attr": "STA"
+            },
+            "number": "15",
+            "image_url": "https://cards.scryfall.io/large/front/8/1/8118282a-1473-4c0b-a283-1f58e0d0209a.jpg?1638111985",
+            "foil": {
+              "is_foil": true,
+              "name": "Foil"
+            },
+            "promotype": {
+              "id": 2,
+              "name": "日本画ミスティカルアーカイブ"
+            }
+          },
+          "condition": "EX+",
+          "lang": "JP",
+          "quantity": 1
+        },
+        "shipment": 1,
+        "single_price": 280,
+        "total_price": 280,
+        "coupon_discount_amount": 0,
+        "isRegistered": false
+      }
+    ]
+  },
+  {
+    "order_id": "order_B4mNqXz8Lp2HrY7KdTfU6W",
+    "buyer_name": "高橋 恒一",
+    "zip_code": "530-0001",
+    "address": "大阪府大阪市北区梅田3-1-1",
+    "shipping_fee": 230,
+    "total_price": 2130,
+    "items": [
+      {
+        "stock": {
+          "id": 5182,
+          "card": {
+            "name": "渦まく知識",
+            "exp": {
+              "name": "メルカディアン・マスクス",
+              "attr": "MMQ"
+            },
+            "number": "61",
+            "image_url": "https://cards.scryfall.io/large/front/9/f/9ff71d13-c4b7-4125-ab10-db4abbb7a074.jpg?1562382082",
+            "foil": {
+              "is_foil": false,
+              "name": ""
+            },
+            "promotype": {
+              "id": 1,
+              "name": ""
+            }
+          },
+          "condition": "NM-",
+          "lang": "EN",
+          "quantity": 3
+        },
+        "shipment": 2,
+        "single_price": 350,
+        "total_price": 1050,
+        "coupon_discount_amount": 50,
+        "isRegistered": false
+      },
+      {
+        "stock": {
+          "id": 5183,
+          "card": {
+            "name": "Sol Ring",
+            "exp": {
+              "name": "Commander Masters",
+              "attr": "CMM"
+            },
+            "number": "396",
+            "image_url": "https://cards.scryfall.io/large/front/a/6/a6d5e716-e939-4456-84e3-2ea22e8c7377.jpg?1734863703",
+            "foil": {
+              "is_foil": true,
+              "name": "Foil"
+            },
+            "promotype": {
+              "id": 1,
+              "name": ""
+            }
+          },
+          "condition": "EX",
+          "lang": "CT",
+          "quantity": 1
+        },
+        "shipment": 1,
+        "single_price": 1080,
+        "total_price": 1080,
+        "coupon_discount_amount": 0,
+        "isRegistered": true
+      }
+    ]
+  },
+  {
+    "order_id": "order_C7vJdPw5Rq9XeK2MnLsA3B",
+    "buyer_name": "中村 遥",
+    "zip_code": "460-0008",
+    "address": "愛知県名古屋市中区栄3-16-1",
+    "shipping_fee": 120,
+    "total_price": 760,
+    "items": [
+      {
+        "stock": {
+          "id": 6391,
+          "card": {
+            "name": "Swords to Plowshares",
+            "exp": {
+              "name": "Double Masters 2022",
+              "attr": "2X2"
+            },
+            "number": "280",
+            "image_url": "https://cards.scryfall.io/large/front/0/6/06554274-78ee-4bfc-b203-b81cb4639427.jpg?1748705114",
+            "foil": {
+              "is_foil": false,
+              "name": ""
+            },
+            "promotype": {
+              "id": 1,
+              "name": ""
+            }
+          },
+          "condition": "NM",
+          "lang": "JP",
+          "quantity": 2
+        },
+        "shipment": 1,
+        "single_price": 180,
+        "total_price": 360,
+        "coupon_discount_amount": 0,
+        "isRegistered": false
+      },
+      {
+        "stock": {
+          "id": 6392,
+          "card": {
+            "name": "Path to Exile",
+            "exp": {
+              "name": "Modern Masters 2017",
+              "attr": "MM3"
+            },
+            "number": "24",
+            "image_url": "https://cards.scryfall.io/png/front/e/8/e8f5d7bb-0c9f-4d9a-9c55-6c0a9cbddf84.png",
+            "foil": {
+              "is_foil": false,
+              "name": ""
+            },
+            "promotype": {
+              "id": 1,
+              "name": ""
+            }
+          },
+          "condition": "NM-",
+          "lang": "EN",
+          "quantity": 1
+        },
+        "shipment": 1,
+        "single_price": 200,
+        "total_price": 200,
+        "coupon_discount_amount": 20,
+        "isRegistered": false
+      },
+      {
+        "stock": {
+          "id": 6393,
+          "card": {
+            "name": "選択",
+            "exp": {
+              "name": "ドミナリア",
+              "attr": "DOM"
+            },
+            "number": "60",
+            "image_url": "https://cards.scryfall.io/large/front/e/7/e7aa556e-1b4f-44a4-aadd-0d248d1c3071.jpg?1645614328",
+            "foil": {
+              "is_foil": false,
+              "name": ""
+            },
+            "promotype": {
+              "id": 1,
+              "name": ""
+            }
+          },
+          "condition": "PLD",
+          "lang": "CT",
+          "quantity": 1
+        },
+        "shipment": 1,
+        "single_price": 80,
+        "total_price": 80,
+        "coupon_discount_amount": 0,
+        "isRegistered": false
+      }
+    ]
+  }
+]);
 
     /**
      * インポート実行
@@ -97,20 +344,55 @@
             <FileUpload type="csv" @action="uploadFile"/>
         </div>
     </v-form>
-    <div class="mt-1 ui grid">
-        <div class="ui form" v-if="hasResult">
-                <div class="two fields">
-                    <div class="nine wide column field">
-                        <label>発送日</label>
-                        <scdatepicker v-model="shiptDate"></scdatepicker>
-                    </div>
-                    <div class="seven wide column field">
-                        <label style="visibility: hidden">インポートボタン</label>
-                            <ModalButton  @action="post()">インポート</ModalButton>
-                        </div>
-                </div>
-        </div>
-    </div>
+    <section class="mt-10" v-if="hasResult">
+        <h2 class="text-title-medium"
+        >
+           検索結果： {{ currentList.length }}件
+        </h2>
+        <v-form class="form_sheet pt-6 pl-6">
+                <v-row>
+                    <v-col cols="3">
+                        <scdatepicker v-model:selectedDate="shiptDate" datelabel="発送日"></scdatepicker>
+                    </v-col>
+                    <v-col>
+                        <ModalButton  @action="post()">インポート</ModalButton>
+                    </v-col>
+                </v-row>
+        </v-form>
+        <article>
+            <v-sheet class="pa-4 mt-4" border rounded v-for="r in currentList" :key="r.order_id">
+                <span class="text-body-small">{{ r.order_id }}</span>
+                <v-row class="mt-3">
+                    <v-col cols="5">
+                        <address id="buyer" class="pa-3 text-body-medium form_sheet">
+                            <span>〒{{ r.zip_code }}</span>
+                            <p>{{ r.address }}</p>
+                            <span class="text-title-medium font-weight-bold">{{ r.buyer_name }}様</span>
+                        </address>
+                    </v-col>
+                    <v-col>
+                        <dl id="price">
+                            <div class="list" v-if="r.coupon_discount_amount != 0">
+                                <dt>クーポン割引</dt>
+                                <dd>
+                                    <span class="mdi mdi-minus"></span>
+                                    ¥{{ r.coupon_discount_amount }}
+                                </dd>
+                            </div>
+                            <div class="list">
+                                <dt>送料</dt>
+                                <dd>¥{{ r.shipping_fee }}</dd>
+                            </div>
+                            <div class="list">
+                                <dt>合計金額</dt>
+                                <dd>¥{{ r.total_price }}</dd>
+                            </div>
+                        </dl>
+                    </v-col>
+                </v-row>
+            </v-sheet>
+        </article>
+    </section>
     <div class="mt-2" v-if="hasResult">
         <div class="ui grid segment" v-for="(r, index) in result.value" :key="index" style="padding:1rem">
             <div class="pl-0">
@@ -175,7 +457,6 @@
 </template>
 
 <style scoped>
-
 .ui.grid > table {
     padding: 0;
 }
@@ -186,13 +467,6 @@
 
 #buyer > p {
     line-height: 1rem;
-    color: #444;
-}
-
-#buyer > .name {
-    font-weight: 700;
-    font-size: 1.2rem;
-    margin-top: 1rem;
 }
 
 #price .list {
@@ -200,7 +474,6 @@
   margin-bottom: 0.5rem;
 }
 #price .list dt {
-    font-weight: 700;
     text-align: right;
     width: 30%;
 }
