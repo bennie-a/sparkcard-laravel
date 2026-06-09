12c480498dcefc29da2b96d205363b4178388101
Aiko Beniyama <saltory72@gmail.com>
2026-06-09 10:13:55 +0900

    :wrench: #322 ショーケース版の割り振りを修正。

--- app/Services/Specific/MshSpCardDetector.php
+++ app/Services/Specific/MshSpCardDetector.php
@@ -2,18 +2,26 @@
 namespace App\Services\Specific;
 
 use App\Services\json\AbstractCard;
+use Override;
 
 /**
  * 『マーベル スーパー・ヒーローズ[MSH]』の特別版
  * を判別するクラス
  */
 class MshSpCardDetector extends DefaultSpCardDetector {
-    public function borderless(AbstractCard $card):string {
+
+    #[Override]
+    public function showcase(string $frame, AbstractCard $card): string
+    {
         $number = $card->number();
         // パネルカード
         if ($number >= 297 && $number <= 313) {
-            return 'postercard';
+            return 'poster';
         }
+        return parent::showcase($frame, $card);
+    }
+    public function borderless(AbstractCard $card):string {
+        $number = $card->number();
         // 計略カード
         if ($number >= 314 && $number <= 351) {
             return 'strategy';
