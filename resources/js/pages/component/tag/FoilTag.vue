84bfefce2fd57292b7612d3a1a4e08d156572302
Aiko Beniyama <saltory72@gmail.com>
2026-06-10 14:13:03 +0900

    :art: #318 Foilタグのレイアウトを変更。

--- resources/js/pages/component/tag/FoilTag.vue
+++ resources/js/pages/component/tag/FoilTag.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-chip variant="flat" label density="compact" size="small"
-        v-if="isFoil" :class="foilClass(foiltype)">
+    <v-chip density="compact" size="small" label variant="flat"
+        v-if="isFoil" :color="foilClass(foiltype)">
         <v-icon icon="mdi-creation" class="mr-1"></v-icon>
         {{ foiltype }}
         </v-chip>
@@ -12,8 +12,8 @@ defineProps({
 });
 const foilClass = (name) => {
     if (name == "Foil") {
-        return "bg-yellow-darken-2";
+        return "purple-darken-2";
     }
-    return "bg-pink-darken-2";
+    return "pink-darken-2";
 };
 </script>
