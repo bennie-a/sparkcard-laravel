0fe38c23376b1bd201f08080a104b0b844a127b0
Aiko Beniyama <saltory72@gmail.com>
2026-06-10 12:13:15 +0900

    :wastebasket: #317 不要なライブラリの読み込みを削除。

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,15 +2,14 @@ import { createApp } from "vue/dist/vue.esm-bundler";
 import App from "./component/App.vue";
 import router from "./router";
 // import "semantic-ui-css/semantic.min.css";
-import "semantic-ui-css/semantic.min.js";
-import Paginate from "vuejs-paginate-next";
+// import "semantic-ui-css/semantic.min.js";
+// import Paginate from "vuejs-paginate-next";
 import VuePapaParse from "vue-papa-parse";
-const app = createApp(App);
 import { store } from "./store.js";
-import Encoding from "encoding-japanese";
+// import Encoding from "encoding-japanese";
 import Datepicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
-import Loading from "vue-loading-overlay";
+// import Loading from "vue-loading-overlay";
 import "vue-loading-overlay/dist/css/index.css";
 import {createPinia} from 'pinia';
 import 'vuetify/styles'
@@ -21,15 +20,15 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { VDateInput } from 'vuetify/labs/VDateInput'
 
+const app = createApp(App);
 
 // ルータをインストール
 app.use(router);
-app.use(Paginate);
 app.use(store);
 app.use(VuePapaParse);
-app.use(Encoding);
-app.use(Datepicker);
-app.use(Loading);
+// app.use(Encoding);
+// app.use(Datepicker);
+// app.use(Loading);
 
 const vuetify = createVuetify({
   components:{
--- resources/js/layout/DefaultLayout.vue
+++ resources/js/layout/DefaultLayout.vue
@@ -2,31 +2,15 @@
 import { ref, onMounted, watch, nextTick, computed } from 'vue'
 import { useRoute } from 'vue-router'
 import SideMenu from "../pages/component/SideMenu.vue"
-import Loading from "vue-loading-overlay"
-import "vue-loading-overlay/dist/css/index.css"
 import AppBreadcrumb from './AppBreadcrumb.vue'
-import { LoadingStore } from '@/stores/loading/Loading'
 import { storeToRefs } from "pinia";
 import MessageArea from '../pages/component/msg/MessageArea.vue'
-
-// ----------------------
-// store
-// ----------------------
-const loadingStore = LoadingStore()
-const { isLoading } = storeToRefs(loadingStore)
 
 // ----------------------
 // route
 // ----------------------
 const route = useRoute()
-
-// ----------------------
-// DOM refs
-// ----------------------
-const sidebar = ref(null)
-const main = ref(null)
 </script>
-
 <template>
     <v-layout>
         <v-app-bar>
@@ -44,26 +28,6 @@ const main = ref(null)
         </v-navigation-drawer>
         <v-main class="mt-0 d-flex  justify-start" min-height="100vh">
         <v-container>
-            <v-breadcrumbs v-if="route.meta.breads" class="pl-0 mb-1">
-                <template
-                    v-for="(title, index) in route.meta.breads"
-                    :key="index"
-                >
-                    <v-breadcrumbs-item color="grey-darken-1">
-                    {{ title }}
-                    </v-breadcrumbs-item>
-
-                    <v-icon
-                    v-if="index < route.meta.breads.length"
-                    icon="mdi-chevron-right"
-                    size="small"
-                     color="grey-darken-1"
-                    />
-                </template>
-                <v-breadcrumbs-item color="grey-darken-1">
-                    {{ route.meta.title }}
-                </v-breadcrumbs-item>
-            </v-breadcrumbs>
             <AppBreadcrumb></AppBreadcrumb>
             <h1 class="text-headline-medium">{{ route.meta.title }}</h1>
             <MessageArea></MessageArea>
@@ -73,67 +37,9 @@ const main = ref(null)
         </v-container>
     </v-main>
     </v-layout>
-
-
-  <!-- <div id="contents" class="ui grid padded">
-    <nav
-      ref="sidebar"
-      class="three wide column blue"
-      :style="{ height: higherHeightPx }"
-    >
-      <SideMenu />
-    </nav>
-
-    <main id="main" ref="main" class="twelve wide column">
-      <nav v-if="route.meta.urls" class="ui breadcrumb">
-        <span v-for="url in route.meta.urls" :key="url">
-          <router-link :to="url.url" class="section">
-            {{ url.title }}
-          </router-link>
-          <i class="right angle icon divider"></i>
-        </span>
-        <span class="active section">{{ route.meta.title }}</span>
-      </nav>
-
-      <h1 class="ui header">
-        {{ route.meta.title }}
-        <div class="sub header">
-          {{ route.meta.description }}
-        </div>
-      </h1>
-
-      <section class="mt-2 ui segment">
-        <router-view />
-      </section>
-
-       <Loading
-        :active="isLoading"
-        :can-cancel="false"
-        :is-full-page="true"
-      />
-    </main>
-  </div>  -->
 </template>
 
 <style scoped>
-/* .ui.grid {
-  height: 100%;
-  min-height: 100%;
-}
-
-.ui.grid .blue.column {
-  margin-right: 3em;
-  padding-left: 0;
-  padding-right: 0;
-  background-color: #1e50a2 !important;
-  height: 100vh;
-  padding-top: 0;
-}
-
-#contents {
-  background-color: whitesmoke;
-} */
-
 .v-container {
   max-width: 1150px !important;
 }
