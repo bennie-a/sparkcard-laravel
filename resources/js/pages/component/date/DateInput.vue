5462810601b425b8824f79f68f54ceef6dd79534
Aiko Beniyama <saltory72@gmail.com>
2026-06-25 10:37:42 +0900

    :recycle: #318 日付選択コンポーネント 不要なコードを削除。

--- resources/js/pages/component/date/DateInput.vue
+++ resources/js/pages/component/date/DateInput.vue
@@ -3,23 +3,17 @@ import { ref} from "vue";
 const selectedDate = defineModel('selectedDate', {type:Date, required:true});
 defineProps({'datelabel': {type:String, required:true}});
 
-// 日付をYYYY/MM/dd形式に整形する。
-const dateFormat = (date) => {
-    const year = date.getFullYear();
-            const month = String(date.getMonth() + 1).padStart(2, "0");
-            const day = String(date.getDate()).padStart(2, "0");
-            return `${year}/${month}/${day}`;
-}
 </script>
 <template>
-    <v-date-input
-    v-model="selectedDate"
-    :label="datelabel"
-    variant="outlined"
-    input-format="yyyy/mm/dd"
-    prepend-icon=""
-    prepend-inner-icon="mdi-calendar-today"
-    />
+    <v-locale-provider locale="ja">
+        <v-date-input
+        v-model="selectedDate"
+        :label="datelabel"
+        variant="outlined"
+        prepend-icon=""
+        prepend-inner-icon="mdi-calendar-today"
+        />
+    </v-locale-provider>
 </template>
 <style>
 .v-date-picker-month__day--selected .v-btn {
