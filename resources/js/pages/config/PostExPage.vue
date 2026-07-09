f6762a2a4d81c7e4147c266a52bcf7ad1b4e39f7
Aiko Beniyama <saltory72@gmail.com>
2026-07-09 11:45:04 +0900

    :art: #317 エキスパンション登録画面 レイアウトをVuetifyに対応。

--- resources/js/pages/config/PostExPage.vue
+++ resources/js/pages/config/PostExPage.vue
@@ -1,118 +1,113 @@
+<script setup>
+    import { AxiosTask } from "../../component/AxiosTask";
+    import MessageArea from "../component/msg/MessageArea.vue";
+    import ModalButton from "../component/modal/ModalButton.vue";
+    import DateInput from "../component/date/DateInput.vue";
+    import { ref } from "vue";
+
+    const name = ref("");
+    const attr = ref("");
+    const releaseDate = ref(new Date());
+    const format = ref("スタンダード");
+    const block = ref("その他");
+
+    const formatOptions = [
+        "スタンダード",
+        "パイオニア",
+        "モダン",
+        "レガシー",
+        "統率者",
+        "マスターピース",
+        "その他",
+    ];
+
+    const store = () => {
+        const task = new AxiosTask(this.$store);
+                let json = {
+                    name: name.value,
+                    attr: attr.value,
+                    block: block.value,
+                    format: format.value,
+                    release_date: releaseDate.value,
+                };
+                const success = function (response, store) {
+                    // this.back();
+                };
+                task.post("/database/exp", json, success);
+                this.$store.dispatch(
+                    "setSuccessMessage",
+                    `${this.name}を登録しました！`
+                );
+    };
+// export default {
+//     data() {
+//         return {
+//             name: "",
+//             attr: "",
+//             block: "サンダー・ジャンクション",
+//             format: "スタンダード",
+//             release_date: null,
+//         };
+//     },
+//     methods: {
+//         release_format: function (date) {
+//             const day = date.getDate();
+//             const month = date.getMonth() + 1;
+//             const year = date.getFullYear();
+//             return `${year}/${month}/${day}`;
+//         },
+//         back: function () {
+//             this.$router.push("/config/expansion");
+//         },
+//         store: function () {
+//             const task = new AxiosTask(this.$store);
+//             let json = {
+//                 name: this.name,
+//                 attr: this.attr,
+//                 block: this.block,
+//                 format: this.format,
+//                 release_date: this.release_date,
+//             };
+//             const success = function (response, store) {
+//                 // this.back();
+//             };
+//             task.post("/database/exp", json, success);
+//             this.$store.dispatch(
+//                 "setSuccessMessage",
+//                 `${this.name}を登録しました！`
+//             );
+//         },
+//     },
+//     components: {
+//         "message-area": MessageArea,
+//         Datepicker: Datepicker,
+//         ModalButton: ModalButton,
+//     },
+// };
+</script>
 <template>
-    <section class="ui grids">
-        <message-area></message-area>
-        <div class="mt-1 content ui form">
-            <div class="two fields">
-                <div class="required six wide field">
-                    <label for="">名称</label>
-                    <input type="text" v-model="name" />
-                </div>
-                <div class="required two wide field">
-                    <label for="">略称</label>
-                    <input type="text" v-model="attr" />
-                </div>
+    <section>
+        <v-form class="form_sheet rounded pa-4 w-50 mx-auto">
+            <v-row class="text-center">
+                <v-col cols="8">
+                    <v-text-field label="名称" v-model="name"></v-text-field>
+                </v-col>
+                <v-col cols="4">
+                    <v-text-field label="略称" v-model="attr"></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="6">
+                    <v-select v-model="format" :items="formatOptions" label="フォーマット">
+                    </v-select>
+                </v-col>
+                <v-col cols="6">
+                    <date-input v-model:selectedDate="releaseDate" datelabel="発売日"></date-input>
+                </v-col>
+            </v-row>
+            <div class="mt-6 text-center">
+                <ModalButton @action="store">登録する</ModalButton>
             </div>
-            <div class="two fields">
-                <div class="required four wide field">
-                    <label for="">ブロック</label>
-                    <select v-model="block" class="ui search dropdown">
-                        <option>タルキール</option>
-                        <option>ミラディン/ファイレクシア</option>
-                        <option>神河</option>
-                        <option>ドミナリア</option>
-                        <option>ラヴニカ</option>
-                        <option>ゼンディカー</option>
-                        <option>イコリア</option>
-                        <option>エルドレイン</option>
-                        <option>イクサラン</option>
-                        <option>ストリクスヘイヴン</option>
-                        <option>イニストラード</option>
-                        <option>カラデシュ</option>
-                        <option>インベイジョン</option>
-                        <option>基本セット</option>
-                        <option>その他</option>
-                    </select>
-                </div>
-                <div class="required four wide field">
-                    <label for="">フォーマット</label>
-                    <select v-model="format" class="ui search dropdown">
-                        <option>スタンダード</option>
-                        <option>パイオニア</option>
-                        <option>モダン</option>
-                        <option>レガシー</option>
-                        <option>統率者</option>
-                        <option>マスターピース</option>
-                        <option>その他</option>
-                    </select>
-                </div>
-            </div>
-            <div class="eight wide field">
-                <label for="">発売日</label>
-                <Datepicker
-                    v-model="release_date"
-                    locale="jp"
-                    input-class-name="pl-2"
-                    auto-apply
-                    :format="release_format"
-                    :enable-time-picker="false"
-                ></Datepicker>
-            </div>
-            <ModalButton @action="store"
-                ><i class="checkmark icon"></i>登録する</ModalButton
-            >
-        </div>
+        </v-form>
     </section>
 </template>
-<script>
-import { AxiosTask } from "../../component/AxiosTask";
-import MessageArea from "../component/msg/MessageArea.vue";
-import Datepicker from "@vuepic/vue-datepicker";
-import ModalButton from "../component/modal/ModalButton.vue";
-
-export default {
-    data() {
-        return {
-            name: "",
-            attr: "",
-            block: "サンダー・ジャンクション",
-            format: "スタンダード",
-            release_date: null,
-        };
-    },
-    methods: {
-        release_format: function (date) {
-            const day = date.getDate();
-            const month = date.getMonth() + 1;
-            const year = date.getFullYear();
-            return `${year}/${month}/${day}`;
-        },
-        back: function () {
-            this.$router.push("/config/expansion");
-        },
-        store: function () {
-            const task = new AxiosTask(this.$store);
-            let json = {
-                name: this.name,
-                attr: this.attr,
-                block: this.block,
-                format: this.format,
-                release_date: this.release_date,
-            };
-            const success = function (response, store) {
-                // this.back();
-            };
-            task.post("/database/exp", json, success);
-            this.$store.dispatch(
-                "setSuccessMessage",
-                `${this.name}を登録しました！`
-            );
-        },
-    },
-    components: {
-        "message-area": MessageArea,
-        Datepicker: Datepicker,
-        ModalButton: ModalButton,
-    },
-};
-</script>
-<style></style>
