cef1aa3263ead87823ca279532bdc4a830719e65
Aiko Beniyama <saltory72@gmail.com>
2026-07-15 11:04:27 +0900

    :construction: #317 カード情報一括登録画面 Vuetify + Composition API形式でリファクタリング中。

--- resources/js/pages/config/CardInfoBulkPage.vue
+++ resources/js/pages/config/CardInfoBulkPage.vue
@@ -1,33 +1,193 @@
+<script setup>
+    import Loading from "vue-loading-overlay";
+    import FileUpload from "../component/FileUpload.vue";
+    import ListPagination from "../component/pagination/ListPagination.vue";
+    import ModalButton from "../component/modal/ModalButton.vue";
+    import { AxiosTask } from "../../component/AxiosTask";
+    import FoilTag from "../component/tag/FoilTag.vue";
+    import PromoDropdown from "../component/selection/PromoDropdown.vue";
+    import ColorDropdown from "../component/selection/ColorDropdown.vue";
+
+    import {ref} from 'vue';
+    import axios from "axios";
+
+    const isDraftOnly = ref(false);
+    const color = ref("");
+// // export default {
+// //     components: {
+// //         "file-upload": FileUpload,
+// //         "loading":Loading,
+// //         pagination: ListPagination,
+// //         ModalButton: ModalButton,
+// //         foiltag: FoilTag,
+// //         promo:PromoDropdown
+// //     },
+// //     data() {
+// //         return {
+// //             filename: "ファイルを選択してください",
+// //             attr: ref(this.$route.query.attr),
+// //             setname:ref(this.$route.query.setname),
+// //             isSkip: false,
+// //             isLoading: false,
+// //             isDraftOnly: false,
+// //             color: "",
+// //             promoItems:[],
+// //             name:ref("通常版"),
+// //             checkedCard:[]
+// //         };
+// //     },
+// //     computed: {
+// //         getCards: function () {
+// //             return this.$store.getters.sliceCard;
+// //         },
+// //         colortext: function () {
+// //             return function (key) {
+// //                 const colors = {
+// //                     W: "白",
+// //                     B: "黒",
+// //                     U: "青",
+// //                     R: "赤",
+// //                     G: "緑",
+// //                     M: "多色",
+// //                     L: "無色",
+// //                     A: "アーティファクト",
+// //                     Land: "土地",
+// //                 };
+// //                 return colors[key];
+// //             };
+// //         },
+// //         join: function () {
+// //             return function (key) {
+// //                 return key.join("|");
+// //             };
+// //         },
+// //         colorlabel: function () {
+// //             return function (key) {
+// //                 const colors = {
+// //                     W: "",
+// //                     B: "black",
+// //                     U: "blue",
+// //                     R: "red",
+// //                     G: "green",
+// //                     M: "orange",
+// //                     L: "purple",
+// //                     A: "grey",
+// //                     Land: "brown",
+// //                 };
+// //                 return colors[key];
+// //             };
+// //         },
+// //     },
+
+// //     methods: {
+// //         upload: async function (file) {
+// //             this.isLoading = true;
+// //             this.filename = file.name;
+// //             const config = {
+// //                 headers: {
+// //                     "Content-Type": "application/json",
+// //                 },
+// //             };
+// //             let query = "?isDraft=" + this.isDraftOnly + "&color=" + this.color+"&setcode=" + this.attr;
+
+// //             await axios
+// //                 .post("/api/upload/card" + query, file, config)
+// //                 .then((response) => {
+// //                     if (response.status == 201) {
+// //                         let item = response.data;
+// //                         this.setCode = item.setCode;
+// //                         this.$store.dispatch("setCard", item.cards);
+// //                         this.checkedCard = item.cards.map(c => c.number);
+// //                     }
+// //                 })
+// //                 .catch((e) => {
+// //                     let status = e.response.status;
+// //                     if (status == 422) {
+// //                         const errors = e.response.data.errors;
+// //                         let msgs = "<ul>";
+// //                         for (let key in errors) {
+// //                             console.log(key);
+// //                             let array = errors[key];
+// //                             array.forEach((msg) => {
+// //                                 msgs += `<li>${msg}</li>`;
+// //                             });
+// //                         }
+// //                         msgs += "</ul>";
+// //                         this.$store.dispatch("message/errorhtml", msgs);
+// //                     } else {
+// //                         const msgs = e.response.data.detail;
+// //                         console.log(msgs);
+// //                         this.$store.dispatch("message/errorhtml", msgs);
+// //                     }
+// //                 })
+// //                 .finally(() => {
+// //                     this.isLoading = false;
+// //                 });
+// //         },
+// //         store: async function () {
+// //             this.isLoading = true;
+// //             this.$store.dispatch("message/clear");
+// //             this.$store.dispatch("clearMessage");
+
+// //             if (this.checkedCard.length == 0) {
+// //                 this.isLoading = false;
+// //                 this.$store.dispatch("message/error", "登録するカードを選択してください。");
+// //                 return;
+// //             }
+
+// //             const task = new AxiosTask(this.$store);
+// //             const list = this.$store.getters.card;
+// //             await Promise.all(
+// //                 list.map(async (card) => {
+// //                     if (this.checkedCard.includes(card.number) == false) {
+// //                         return;
+// //                     }
+// //                     if (card.name != "") {
+// //                         const success = function (response, store) {};
+// //                         card["is_skip"] = this.isSkip;
+// //                         await task.post("/database/card", card, success);
+// //                     }
+// //                 })
+// //             ).catch(() => {
+// //                 console.error("error");
+// //             });
+// //             this.isLoading = false;
+// //             this.$store.dispatch(
+// //                 "setSuccessMessage",
+// //                 `${this.checkedCard.length}件登録が完了しました。`
+// //             );
+
+// //             console.log("store finished.");
+// //         },
+// //     },
+// };
+</script>
 <template>
-    <message-area></message-area>
-    <div v-if="setname != ''">
+    <v-form rounded class="form_sheet pa-4">
+        <v-row  gap="10" class="align-center">
+            <v-col cols="2" class="mr-5">
+                <v-switch v-model="isDraftOnly" label="通常版のみ表示" color="teal-lighten-1" true-icon="mdi-check"
+            false-icon="mdi-close"></v-switch>
+            </v-col>
+            <v-col cols="3" class="mr-5">
+                <color-dropdown v-model="color"></color-dropdown>
+            </v-col>
+            <v-col cols="5">
+                <file-upload @action="upload" type="json" icon="mdi-code-json"></file-upload>
+            </v-col>
+        </v-row>
+    </v-form>
     <article class="mt-1 ui grid segment">
         <div
-            class="three wide column middle aligned content ui toggle checkbox"
+        class="three wide column middle aligned content ui toggle checkbox"
         >
-            <input type="checkbox" id="isDraftOnly" v-model="isDraftOnly" />
-            <label for="isDraftOnly">通常版のみ表示</label>
         </div>
         <div class="three wide column field">
-            <select v-model="color" class="ui dropdown">
-                <option value="">全て</option>
-                <option value="R">赤</option>
-                <option value="W">白</option>
-                <option value="B">黒</option>
-                <option value="G">緑</option>
-                <option value="U">青</option>
-                <option value="M">多色</option>
-                <option value="L">無色</option>
-                <option value="A">アーティファクト</option>
-                <option value="Land">土地</option>
-                <option value="Art">アート・カード</option>
-            </select>
         </div>
         <div class="eight wide column">
-            <file-upload @action="upload" type="json" icon="mdi-code-json"></file-upload>
         </div>
     </article>
-    <article class="mt-1" v-if="getCards.length != 0">
+    <!-- <article class="mt-1" v-if="getCards.length != 0">
         <div class="ui large form mt-2" v-if="$store.getters.isLoad == false">
             <div class="field">
                 <table class="ui table striped six column">
@@ -86,189 +246,14 @@
                 </div>
             </div>
         </div>
-    </article>
-</div>
+    </article> -->
     <loading
         :active="isLoading"
         :can-cancel="false"
         :is-full-page="true"
     ></loading>
 </template>
-<script>
-import Loading from "vue-loading-overlay";
-import FileUpload from "../component/FileUpload.vue";
-import MessageArea from "../component/msg/MessageArea.vue";
-import ListPagination from "../component/pagination/ListPagination.vue";
-import ModalButton from "../component/modal/ModalButton.vue";
-import { AxiosTask } from "../../component/AxiosTask";
-import FoilTag from "../component/tag/FoilTag.vue";
-import PromoDropdown from "../component/selection/PromoDropdown.vue";
-import {ref} from 'vue';
 
-import axios from "axios";
-export default {
-    components: {
-        "file-upload": FileUpload,
-        "message-area": MessageArea,
-        "loading":Loading,
-        pagination: ListPagination,
-        ModalButton: ModalButton,
-        foiltag: FoilTag,
-        promo:PromoDropdown
-    },
-    data() {
-        return {
-            filename: "ファイルを選択してください",
-            attr: ref(this.$route.query.attr),
-            setname:ref(this.$route.query.setname),
-            isSkip: false,
-            isLoading: false,
-            isDraftOnly: false,
-            color: "",
-            promoItems:[],
-            name:ref("通常版"),
-            checkedCard:[]
-        };
-    },
-    computed: {
-        getCards: function () {
-            return this.$store.getters.sliceCard;
-        },
-        colortext: function () {
-            return function (key) {
-                const colors = {
-                    W: "白",
-                    B: "黒",
-                    U: "青",
-                    R: "赤",
-                    G: "緑",
-                    M: "多色",
-                    L: "無色",
-                    A: "アーティファクト",
-                    Land: "土地",
-                };
-                return colors[key];
-            };
-        },
-        join: function () {
-            return function (key) {
-                return key.join("|");
-            };
-        },
-        colorlabel: function () {
-            return function (key) {
-                const colors = {
-                    W: "",
-                    B: "black",
-                    U: "blue",
-                    R: "red",
-                    G: "green",
-                    M: "orange",
-                    L: "purple",
-                    A: "grey",
-                    Land: "brown",
-                };
-                return colors[key];
-            };
-        },
-    },
-    mounted: async function () {
-        // this.isLoading = true;
-        // await axios.get('/api/database/exp/' + this.setCode, {})
-        // .then((response) => {
-        //                         this.$store.dispatch("clearCards");
-        //                         this.setName = response.data.name;
-        //                     })
-        //                     .catch((e) => {
-        //                         console.error(e.statusCode);
-        //                     })
-        //                     .finally(() => {
-        //                         this.isLoading = false;
-        //                     });
-    },
-    methods: {
-        upload: async function (file) {
-            this.isLoading = true;
-            this.filename = file.name;
-            const config = {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            };
-            let query = "?isDraft=" + this.isDraftOnly + "&color=" + this.color+"&setcode=" + this.attr;
-
-            await axios
-                .post("/api/upload/card" + query, file, config)
-                .then((response) => {
-                    if (response.status == 201) {
-                        let item = response.data;
-                        this.setCode = item.setCode;
-                        this.$store.dispatch("setCard", item.cards);
-                        this.checkedCard = item.cards.map(c => c.number);
-                    }
-                })
-                .catch((e) => {
-                    let status = e.response.status;
-                    if (status == 422) {
-                        const errors = e.response.data.errors;
-                        let msgs = "<ul>";
-                        for (let key in errors) {
-                            console.log(key);
-                            let array = errors[key];
-                            array.forEach((msg) => {
-                                msgs += `<li>${msg}</li>`;
-                            });
-                        }
-                        msgs += "</ul>";
-                        this.$store.dispatch("message/errorhtml", msgs);
-                    } else {
-                        const msgs = e.response.data.detail;
-                        console.log(msgs);
-                        this.$store.dispatch("message/errorhtml", msgs);
-                    }
-                })
-                .finally(() => {
-                    this.isLoading = false;
-                });
-        },
-        store: async function () {
-            this.isLoading = true;
-            this.$store.dispatch("message/clear");
-            this.$store.dispatch("clearMessage");
-
-            if (this.checkedCard.length == 0) {
-                this.isLoading = false;
-                this.$store.dispatch("message/error", "登録するカードを選択してください。");
-                return;
-            }
-
-            const task = new AxiosTask(this.$store);
-            const list = this.$store.getters.card;
-            await Promise.all(
-                list.map(async (card) => {
-                    if (this.checkedCard.includes(card.number) == false) {
-                        return;
-                    }
-                    if (card.name != "") {
-                        const success = function (response, store) {};
-                        card["is_skip"] = this.isSkip;
-                        await task.post("/database/card", card, success);
-                    }
-                })
-            ).catch(() => {
-                console.error("error");
-            });
-            this.isLoading = false;
-            this.$store.dispatch(
-                "setSuccessMessage",
-                `${this.checkedCard.length}件登録が完了しました。`
-            );
-
-            console.log("store finished.");
-        },
-    },
-};
-</script>
 <style>
 .wall {
     background-color: white;
